3903055a8b12cd42a3bf0f98fe4fd08d0bcfaa0a
AyoubBakkoury <45005090+AyoubBakkoury@users.noreply.github.com>
2021-06-03 12:40:03 +0200

    Version complete de la demo

--- demo_streamlit.py
+++ demo_streamlit.py
@@ -1,62 +1,96 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jun  3 11:28:30 2021
+Created on Thu May 27 18:42:23 2021
 
-@author: ayoub
+@author: Ay
 """
 
-import streamlit as st 
-import pandas as pd 
-from sklearn.model_selection import train_test_split 
+import streamlit as st
+import pandas as pd
 
-from preprocess import preprocess , modeling
-
-
-df = pd.read_csv('/Users/ayoub/Documents/GitHub/demo_streamlit_MAI21_BCDADS/train.csv')
-
-pages= ['Présentation', 'Modélisation']
-
-partie  = st.sidebar.radio('Parties', pages)
+from sklearn.model_selection import train_test_split
 
 
-if partie == pages[0]:
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from preprocessing import generate_train_data
+from modelisation import get_model
+
+page = st.sidebar.radio("", options = ['Présentation', 'Modélisation']) 
+
+df = pd.read_csv("train.csv", index_col = 'PassengerId')
+
+if page == 'Présentation':
+    st.title("Démo Streamlit Mar21 DA DS")
     
-    ##présentation 
-    st.title('Données Titanic')
-    st.write(df.head())
-
-
-else: 
-
-    st.title('Modélisation')
-
-
-    X, y = preprocess()
-
-#st.write(X.isna().sum())
-
-
-
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 1)
-
-
-
-    options = ['Regression log', 'KNN']
-
-    choix = st.radio('Choisissez un modèle', options)
+    st.markdown("""
+                Ce projet va entraîner un modèle de Machine Learning
+                sur le dataset du [titanic](https://www.kaggle.com/c/titanic/overview).
+                
+                                
+                
+                """)
     
-    score = modeling(choix, X_train, y_train, X_test, y_test)
- 
+    img = plt.imread("assets/titanic.jpg")
     
-    st.write('Score pour', choix, ':', score)  
+    st.image(img)
+    
+    sns.countplot(df['Survived'])
+    
+    fig = plt.gcf()
+
+    st.pyplot(fig) 
     
     
     
+    st.markdown("""
+                Voici un aperçu du dataset.
+                
+                """)
+    st.write(df)
+
+if page == 'Modélisation':
     
+    # Import et nettoyage des données
     
+    X, y = generate_train_data()
     
+    # Split des données
     
+    X_train, X_test, y_train, y_test = train_test_split(X, y,test_size = 0.2, random_state = 1)
     
+    # Entrainement du modèle
     
+    options = ['Regression Logistique', 'KNN', 'Decision Tree']
+    
+    choix = st.radio("Choisissez un modèle", options = options) 
+    
+    model, score = get_model(choix, X_train, y_train, X_test, y_test)
+           
+        
+    st.markdown("""
+                Ce modèle a obtenu un score de :
+                
+                """)    
+    st.write(score)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
